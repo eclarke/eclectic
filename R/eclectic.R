4d0467e7db18e8c4f362b2f030e5a5b191e04142
Erik Clarke <ecl@mail.med.upenn.edu>
2015-09-10 10:37:33 -0400

    updated exports

--- R/eclectic.R
+++ R/eclectic.R
@@ -1,3 +1,6 @@
+#' Default BLAST outfmt 6 column names
+#'
+#' @export
 blast6_colnames <- c(
   "qseqid","sseqid","pident","length","mismatch","gapopen","qstart","qend",
   "sstart","send","evalue","bitscore")
@@ -7,7 +10,8 @@ blast6_colnames <- c(
 #' @param fp file path to blast output
 #' @param col.names the column names of the BLAST output. The "std" columns are
 #' defined in `blast6_colnames` (default)
-read_blast6 <- function(fp, col.names=eclectic::blast6.colnames) {
+#' @export
+read_blast6 <- function(fp, col.names=blast6.colnames) {
   infile <- read.delim(fp, col.names=col.names, stringsAsFactors = FALSE)
   return(infile)
 }
