b5bbefe8680458a166b258163f8fbecbe93e8412
Erik Clarke <ecl@mail.med.upenn.edu>
2015-09-10 10:38:15 -0400

    grr

--- R/eclectic.R
+++ R/eclectic.R
@@ -11,7 +11,7 @@ blast6_colnames <- c(
 #' @param col.names the column names of the BLAST output. The "std" columns are
 #' defined in `blast6_colnames` (default)
 #' @export
-read_blast6 <- function(fp, col.names=blast6.colnames) {
+read_blast6 <- function(fp, col.names=blast6_colnames) {
   infile <- read.delim(fp, col.names=col.names, stringsAsFactors = FALSE)
   return(infile)
 }
