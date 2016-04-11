58bd284c4909a04991931d128da5940a157ef821
Erik Clarke <ecl@mail.med.upenn.edu>
2016-04-11 18:33:17 -0400

    added subcolor_pal for grouped factor coloring

--- R/eclectic.R
+++ R/eclectic.R
@@ -272,3 +272,45 @@ tax_climber <- function(otus, taxonomy, end="Genus", label=FALSE, sep=":") {
   else
     lowest
 }
+
+#' @title Grouped color palette for factors
+#' @description
+#'    Picks a starting hue for each level of the factor, then returns colors
+#'    with that hue, but varying luminosity for each item within that factor.
+#' @param fctr the factor to create color scale for
+#' @param h the range of hues to use
+#' @param c chroma value to use
+#' @param l the base luminance [0,100]
+#' @param l.range the luminance above and below the base to use for subcolors
+#' @param h.start the starting hue
+#' @param direction 1=clockwise, 0=counterclockwise around color wheel
+#' @return a named vector of hexadecimal colors
+#' @export
+subcolor_pal <- function (fctr, h = c(0, 360) + 15, c = 100, l=65, l.range=20, h.start = 0, direction = 1)
+{
+  stopifnot(l-l.range >= 0)
+  stopifnot(l+l.range <= 100)
+  .fctr <- as.factor(fctr)
+  n = length(levels(.fctr))
+  if ((diff(h)%%360) < 1) {
+    h[2] <- h[2] - 360/n
+  }
+  rotate <- function(x) (x + h.start)%%360 * direction
+  hues <- rotate(seq(h[1], h[2], length.out = n))
+  levels(.fctr) <- hues
+  subcolors <- lapply(split(.fctr, .fctr), function(subgroup) {
+    hue = as.integer(as.character(unique(subgroup)))
+    stopifnot(length(hue) == 1)
+
+    sub_n <- length(subgroup)
+
+    if (sub_n > 1)
+      l <- seq(l-l.range, l+l.range, length.out = sub_n)
+
+    grDevices::hcl(hue, c, l)
+  })
+  minor.colors <- unsplit(subcolors, .fctr)
+  names(minor.colors) <- as.character(fctr)
+  minor.colors
+}
+
